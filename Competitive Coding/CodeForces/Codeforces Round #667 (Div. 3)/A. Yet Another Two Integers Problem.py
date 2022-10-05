e54870ebebc9a33d433525eb07c86469896c8372
Alam9835 <alamsa9835@gmail.com>
2022-10-05 13:29:45 +0530

    Added File

--- Competitive Coding/CodeForces/Codeforces Round #667 (Div. 3)/A. Yet Another Two Integers Problem.py
+++
@@ -1,6 +0,0 @@
-t = int(input())
-for _ in range(t):
-    a, b = [int(s) for s in input().split(" ")]
-    ans = (abs(a-b)+9)//10
-    print(ans)
-            
--- Competitive Coding/CodeForces/Codeforces Round #697 (Div. 3)/A. Odd Divisor.py
+++
@@ -1,7 +0,0 @@
-t = int(input())
-for _ in range(t):
-    n = int(input())
-    if bin(n).count("1") == 1:
-        print("NO")
-    else:
-        print("YES")
