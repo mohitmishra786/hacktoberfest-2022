74d94d95eeb1000a0a39e391965e13a97f69e888
Abhishek Krishna <akdpsgaya792@gmail.com>
2022-10-05 20:21:09 +0530

    Create Next_Round.cpp

---
+++ Competitive Coding/CodeForces/Next_Round.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, k;
+    cin >> n >> k;
+
+    int a;
+    for (int i = 0; i < k; ++i)
+    {
+        cin >> a;
+        if (a == 0)
+        {
+            cout << i << endl;
+            return 0;
+        }
+    }
+
+    int b;
+    for (int i = k; i < n; i++)
+    {
+        cin >> b;
+        if (b != a)
+        {
+            cout << i << endl;
+            return 0;
+        }
+    }
+
+    cout << n << endl;
+    return 0;
+}
