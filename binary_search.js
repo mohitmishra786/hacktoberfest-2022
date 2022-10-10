96a2d1b044d73a4ee1859ddac974730aac991e9c
Ademeso josiah <68749841+ademesojosiah@users.noreply.github.com>
2022-10-10 23:24:13 +0100

    added recursive binary search

--- binary_search.js
+++ binary_search.js
@@ -14,17 +14,34 @@ function binary_search(list,target){
     return false
 }
 
+function recursive_binary(list,target){
+    if(list.length === 1 && list[0] !== target){
+        return false
+    }
+    let midpoint = Math.floor((list.length)/2)
+    if(list[midpoint]=== target){
+        return true
+    }else if(target > list[midpoint]){
+        return recursive_binary(list.slice((midpoint)),target)
+
+    }else if(target < list[midpoint]){
+        return recursive_binary(list.slice(0,(midpoint)),target)
+    }
+}
 
 
 const list = [0,1,2,3,4,5,6,7,8,9,10]
 
 
-const result = binary_search(list,70)
+const result = binary_search(list,11)
+const result1 = binary_search(list,70)
 
 function verify(result){
 console.log('target found', result);
 }
 
 verify(result)
+verify(result1)
+
 
 
